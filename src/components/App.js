e2b7d6f8dbf14ff46321e848ea53440de131b8d5
Anurag Joshi <anuragj990@gmail.com>
2024-12-27 16:55:51 +0530

    1st try

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import './../styles/App.css';
 
 const App = (props) => {
 
+  const [state, setState]=useState('hide');
+
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className={state}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={()=> {if(state==='show'){setState('hide')} else{setState('show')}}}>Click me</button>
     </div>
   );
 }
